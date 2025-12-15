53256dbf3cff855bb59cda5b1990558d6b8db259
Andrés Alegría <andresalegria@live.com>
2025-12-15 12:20:33 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export default {
     title: 'Through the Safety Net',
     subtitle:
       'Oil pipeline expansion is putting the planet’s last biodiversity strongholds—and many Indigenous lands—at risk.',
-    date: 'Draft',
+    date: 'Draft Not Published',
     social: [
       {
         name: 'twitter',
